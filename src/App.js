d75e26596b6070f65d1fd9a21ad3bc826ef4f905
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-10-28 00:44:16 +0600

    display user name , email , photo

--- src/App.js
+++ src/App.js
@@ -2,23 +2,42 @@ import { initializeApp } from "firebase/app";
 import {
   getAuth, GoogleAuthProvider, signInWithPopup
 } from "firebase/auth";
+import { useState } from "react";
 import './App.css';
 import firebaseConfig from './Firebase/firebase.config';
+
 initializeApp(firebaseConfig)
-const auth = getAuth()
-const provider = new GoogleAuthProvider();
-const handleSignIn = () => {
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      console.log(result);
-    }).catch((error) => {
-      console.log(error);
-    });
-}
+
 function App() {
+  const [user, setUser] = useState({})
+  const auth = getAuth()
+  const provider = new GoogleAuthProvider();
+  const handleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const { displayName: userName, photoURL: photo, email } = result.user;
+        const loadUSer = {
+          name: userName,
+          email: email,
+          photo: photo
+        }
+        setUser(loadUSer)
+      }).catch((error) => {
+        console.log(error);
+      });
+  }
   return (
     <div className="App">
       <button onClick={handleSignIn}>Sign In With Google</button>
+      <div>
+        {
+          user.email && <div>
+            <h1>Welcome {user.name}</h1>
+            <p>Email : {user.email}</p>
+            <img style={{height:"100px", width:"100px", borderRadius:"50%"}} src={user.photo} alt="" />
+          </div>
+        }
+      </div>
     </div>
   );
 }
