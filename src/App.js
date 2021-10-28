bd09c8af78d5efbb0d8d78f6347ab4ffdcb6a480
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-10-28 13:30:51 +0600

    added github signin & shown info

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import { initializeApp } from "firebase/app";
 import {
-  getAuth, GoogleAuthProvider, signInWithPopup
+  getAuth, GoogleAuthProvider, signInWithPopup, GithubAuthProvider
 } from "firebase/auth";
 import { useState } from "react";
 import './App.css';
 import firebaseConfig from './Firebase/firebase.config';
 
 initializeApp(firebaseConfig)
-
 function App() {
   const [user, setUser] = useState({})
   const auth = getAuth()
-  const provider = new GoogleAuthProvider();
-  const handleSignIn = () => {
-    signInWithPopup(auth, provider)
+  const GoogleProvider = new GoogleAuthProvider();
+  const gitProvider = new GithubAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, GoogleProvider)
+      .then((result) => {
+        const { displayName: userName, photoURL: photo, email } = result.user;
+        const loadUSer = {
+          name: userName,
+          email: email,
+          photo: photo
+        }
+        setUser(loadUSer)
+      }).catch((error) => {
+        console.log(error);
+      });
+  }
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, gitProvider)
       .then((result) => {
         const { displayName: userName, photoURL: photo, email } = result.user;
         const loadUSer = {
@@ -28,13 +43,14 @@ function App() {
   }
   return (
     <div className="App">
-      <button onClick={handleSignIn}>Sign In With Google</button>
+      <button onClick={handleGoogleSignIn}>Sign In With Google</button>
+      <button onClick={handleGithubSignIn}>Sign In With Github</button>
       <div>
         {
           user.email && <div>
             <h1>Welcome {user.name}</h1>
             <p>Email : {user.email}</p>
-            <img style={{height:"100px", width:"100px", borderRadius:"50%"}} src={user.photo} alt="" />
+            <img style={{ height: "100px", width: "100px", borderRadius: "50%" }} src={user.photo} alt="" />
           </div>
         }
       </div>
